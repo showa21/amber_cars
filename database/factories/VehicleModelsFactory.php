173e64e1f97ce558c30b94c3e6ac835be185fd07
showa21 <showaspace@yahoo.com>
2021-09-27 15:41:46 -0500

    seed factory controller migration

---
+++ database/factories/VehicleModelsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\VehicleModels;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VehicleModelsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = VehicleModels::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
